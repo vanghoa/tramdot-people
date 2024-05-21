98c16828cb86935cd3b62d48177f719f59ee633a
Bao Anh Bui <bao@tusitalabooks.com>
2024-05-21 17:12:55 +0800

    cai dit me

---
+++ src/App.js
@@ -0,0 +1,122 @@
+import { useRef, useState } from 'react';
+import './App.css';
+import { SVGBodyArray } from './SVG';
+import * as htmlToImage from 'html-to-image';
+
+function App() {
+    const [text, setText] = useState('a');
+    const [pose, setPose] = useState(0);
+    const imageRef = useRef(null);
+    const poseHandler = () => {
+        if (pose >= SVGBodyArray.length - 1) {
+            setPose(0);
+            return;
+        }
+        setPose((pose) => pose + 1);
+    };
+    const imageHandler = () => {
+        htmlToImage
+            .toJpeg(imageRef.current, {
+                cacheBust: true,
+                backgroundColor: '#baff00',
+            })
+            .then((dataUrl) => {
+                const link = document.createElement('a');
+                link.download = `your-tramdot-people.png`;
+                link.href = dataUrl;
+                link.target = '_blank';
+                link.rel = 'noopener noreferrer';
+                link.click();
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    };
+    return (
+        <main>
+            <input
+                placeholder="Gõ tên"
+                spellCheck="false"
+                maxLength="5"
+                onChange={(e) => setText(e.target.value)}
+            ></input>
+            <section ref={imageRef}>
+                <div className="artwork">
+                    {SVGBodyArray[pose]({ text: text })}
+                </div>
+            </section>
+            <div className="buttonsection">
+                <button onClick={poseHandler}>Different pose</button>
+                <button onClick={imageHandler}>Save image</button>
+            </div>
+        </main>
+    );
+}
+
+export function SVGText({ children = '', style = {}, className = '' }) {
+    const styleArr = children.split('').map((str) => getRandomInteger(-20, 20));
+    return (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            preserveAspectRatio="xMidYMid"
+            className={`${className} head`}
+            viewBox="0 0 400 400"
+            width={400}
+            height={400}
+            style={style}
+        >
+            {Array.from({ length: 4 }).map((e, k) => {
+                return (
+                    <text
+                        key={k}
+                        x="50%"
+                        y="50%"
+                        dominantBaseline="middle"
+                        textAnchor="middle"
+                        vectorEffect="non-scaling-stroke"
+                        className="stroke_default"
+                    >
+                        {children.split('').map((str, k) => {
+                            return (
+                                <tspan key={k} rotate={styleArr[k]}>
+                                    {str}
+                                </tspan>
+                            );
+                        })}
+                    </text>
+                );
+            })}
+        </svg>
+    );
+}
+
+export function SVGPath({ children, className = '', ...rest }) {
+    return (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            preserveAspectRatio="xMidYMid"
+            className={`${className} body`}
+            {...rest}
+        >
+            {Array.from({ length: 4 }).map((e, k) => children)}
+        </svg>
+    );
+}
+
+export function Path({ ...rest }) {
+    return (
+        <path
+            className="stroke_default"
+            x="50%"
+            y="50%"
+            vectorEffect="non-scaling-stroke"
+            fillRule="evenodd"
+            clipRule="evenodd"
+            {...rest}
+        />
+    );
+}
+function getRandomInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+export default App;
